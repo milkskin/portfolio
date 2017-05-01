9d00a2a584e69f690fa058aa110776d69a60c6d5
Hyeonjong <nianelo4@gmail.com>
2017-05-01 12:19:25 +0000

    Add Bootstrap & jQuery

--- application/views/layout/base/header.php
+++ application/views/layout/base/header.php
@@ -2,3 +2,7 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Portfolio</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<noscript><!-- no JS alternative --></noscript>

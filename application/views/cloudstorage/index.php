b4869d61d32199860783aa27d84a9876004df0eb
Hyeonjong <nianelo4@gmail.com>
2017-05-03 15:08:05 +0000

    [Cloud storage] Simplify grid options

--- application/views/cloudstorage/index.php
+++ application/views/cloudstorage/index.php
@@ -1,9 +1,9 @@
-<div class="col-xs-12 col-sm-12 col-md-4">
+<div class="col-xs-12 col-md-4">
 	<h2>jsTree area</h2>
 
 	<div id="jstree_demo_div"><?php echo $dir_list; ?></div>
 </div>
 
-<div class="col-xs-12 col-sm-12 col-md-8">
+<div class="col-xs-12 col-md-8">
 	<h2>File uploader area</h2>
 </div>

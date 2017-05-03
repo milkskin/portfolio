9aa0f05611d6b065f821c526fc68456c71f25860
Hyeonjong <nianelo4@gmail.com>
2017-05-03 07:33:25 +0000

    Add jsTree for [Cloud storage]

--- application/views/cloudstorage/index.php
+++ application/views/cloudstorage/index.php
@@ -1,1 +1,7 @@
+<div class="col-xs-12 col-sm-12 col-md-4">
+	<h2>jsTree area</h2>
+</div>
 
+<div class="col-xs-12 col-sm-12 col-md-8">
+	<h2>File uploader area</h2>
+</div>
--- application/views/layout/base/header.php
+++ application/views/layout/base/header.php
@@ -3,7 +3,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Portfolio</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/themes/default/style.min.css">
 <link rel="stylesheet" href="/assets/style/custom.css">
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/jstree.min.js"></script>
 <noscript><!-- no JS alternative --></noscript>

7501093ef1d5db30d9ee71887bcb9f1ea1eae0c9
Hyeonjong <nianelo4@gmail.com>
2017-05-16 14:23:59 +0000

    Add File-uploader for [Cloud storage]

--- application/views/layout/base/header.php
+++ application/views/layout/base/header.php
@@ -4,8 +4,12 @@
 <title>Portfolio</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/themes/default/style.min.css">
 <link rel="stylesheet" href="/assets/libs/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+<link rel="stylesheet" href="/assets/libs/fileupload/9.18.0/css/jquery.fileupload.css">
+<link rel="stylesheet" href="/assets/libs/fileupload/9.18.0/css/jquery.fileupload-ui.css">
 <link rel="stylesheet" href="/assets/style/custom.css">
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/jstree.min.js"></script>
-<noscript><!-- no JS alternative --></noscript>
+<noscript><link rel="stylesheet" href="/assets/libs/fileupload/9.18.0/css/jquery.fileupload-noscript.css"></noscript>
+<noscript><link rel="stylesheet" href="/assets/libs/fileupload/9.18.0/css/jquery.fileupload-ui-noscript.css"></noscript>

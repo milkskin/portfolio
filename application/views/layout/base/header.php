db1dd53132540ae36113f39b4fbb9585e0a5c34b
Hyeonjong <nianelo4@gmail.com>
2017-05-27 11:45:32 +0000

    [Cloud storage] Add some resources for change all CDN

--- application/views/layout/base/header.php
+++ application/views/layout/base/header.php
@@ -2,17 +2,17 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Portfolio</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/themes/default/style.min.css">
 <link rel="stylesheet" href="/assets/libs/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+<link rel="stylesheet" href="/assets/libs/jstree/3.3.4/dist/themes/default/style.min.css">
+<link rel="stylesheet" href="/assets/libs/fileupload/9.18.0/css/blueimp-gallery.min.css">
 <link rel="stylesheet" href="/assets/libs/fileupload/9.18.0/css/jquery.fileupload.css">
 <link rel="stylesheet" href="/assets/libs/fileupload/9.18.0/css/jquery.fileupload-ui.css">
 <link rel="stylesheet" href="/assets/style/custom.css">
-<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/jstree.min.js"></script>
-<script src="//blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script>
-<script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
+<script src="/assets/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="/assets/libs/ui/1.12.1/jquery-ui.min.js"></script>
+<script src="/assets/libs/jstree/3.3.4/dist/jstree.min.js"></script>
+<script src="/assets/libs/fileupload/9.18.0/js/tmpl.min.js"></script>
+<script src="/assets/libs/fileupload/9.18.0/js/jquery.blueimp-gallery.min.js"></script>
 <script src="/assets/libs/fileupload/9.18.0/js/jquery.iframe-transport.js"></script>
 <script src="/assets/libs/fileupload/9.18.0/js/jquery.fileupload.js"></script>
 <script src="/assets/libs/fileupload/9.18.0/js/jquery.fileupload-process.js"></script>
